24d732fb325b0e9b03bd4c4f6ca240f45e6520ef
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2020-04-28 02:56:52 +0500

    geo loc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { usePosition } from 'use-position';
+
 
 function App() {
+  const watch = true;
+const {
+  latitude,
+  longitude,
+  timestamp,
+  accuracy,
+  error,
+} = usePosition(watch,{enableHighAccuracy: true});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      latitude: {latitude}<br/>
+      longitude: {longitude}<br/>
+      timestamp: {timestamp}<br/>
+      accuracy: {accuracy && `${accuracy}m`}<br/>
+      error: {error}
     </div>
   );
 }
